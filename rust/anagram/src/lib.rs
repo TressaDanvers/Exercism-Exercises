dfff1c4a574032058a2fa38e94a99dce4fd2ca3b
Tressa Danvers <TDanvers@protonmail.ch>
2024-01-27 17:05:27 -0500

    [rust/anagram] first submission

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,5 +1,23 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    HashSet::from_iter(
+        possible_anagrams.iter()
+            .filter(|&&it| { it.to_lowercase() != word.to_lowercase() })
+            .filter_map(|&it| {
+                let mut letters_in_it = it
+                    .to_lowercase()
+                    .chars()
+                    .collect::<Vec<char>>();
+                let mut letters_in_word = word
+                    .to_lowercase()
+                    .chars()
+                    .collect::<Vec<char>>();
+
+                letters_in_it.sort();
+                letters_in_word.sort();
+
+                if letters_in_it == letters_in_word { Some(it) }
+                else { None }
+            }))
 }
