206956e3628dc934efff86dd63d533f54930562c
Tressa Danvers <TDanvers@protonmail.ch>
2024-01-28 15:56:46 -0500

    [rust/space-age] first submission

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -2,34 +2,37 @@
 // In order to pass the tests you can add-to or change any of this code.
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    seconds: u64
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        todo!("s, measured in seconds: {s}")
+        Self { seconds: s }
     }
 }
 
 pub trait Planet {
-    fn years_during(d: &Duration) -> f64 {
-        todo!("convert a duration ({d:?}) to the number of years on this planet for that duration");
+    fn years_during(d: &Duration) -> f64;
+}
+
+macro_rules! impl_planet {
+    ($name:ident,$period:expr) => {
+        pub struct $name;
+
+        impl Planet for $name {
+            fn years_during(d: &Duration) -> f64 {
+                d.seconds as f64 / (($period) * 365.25 * 24.0 * 60.0 * 60.0)
+            }
+        }
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
-
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl_planet!(Mercury, 0.2408467);
+impl_planet!(Venus, 0.61519726);
+impl_planet!(Earth, 1.0);
+impl_planet!(Mars, 1.8808158);
+impl_planet!(Jupiter, 11.862615);
+impl_planet!(Saturn, 29.447498);
+impl_planet!(Uranus, 84.016846);
+impl_planet!(Neptune, 164.79132);
