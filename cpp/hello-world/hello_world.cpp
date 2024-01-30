0f49e6ac27a03a1c72ffbf52b51f9f2ff0b6036a
Tressa Danvers <TDanvers@protonmail.ch>
2024-01-30 18:50:06 -0500

    cleaned [c++/hello-world] submission

--- cpp/hello-world/hello_world.cpp
+++ cpp/hello-world/hello_world.cpp
@@ -1,13 +1,5 @@
 #include "hello_world.h"
 
-using namespace std;
-
-namespace hello_world
-{
-
-string hello()
-{
-    return "Hello, World!";
-}
-
+namespace hello_world {
+    std::string hello() { return "Hello, World!"; }
 }
