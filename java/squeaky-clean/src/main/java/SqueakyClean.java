5ecbc8c73cb6a3459d9ba87b19f27a881d056412
Tressa Danvers <TDanvers@protonmail.ch>
2024-01-30 16:38:51 -0500

    [java/squeaky-clean] second submission

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -9,6 +9,8 @@ class SqueakyClean {
       .mapToObj(c -> (char)c)
       .map(c -> {
         var token = c == ' ' ? "_"
+          : c == '0' ? "o" : c == '1' ? "l" : c == '3' ? "e"
+          : c == '4' ? "a" : c == '7' ? "t"
           : Character.isISOControl(c) ? "CTRL"
           : !Character.isAlphabetic(c) || ('α' <= c && c <= 'ω') ? ""
           : convertCamel.get() ? String.valueOf(Character.toUpperCase(c))
