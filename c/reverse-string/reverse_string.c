9ef9a3837371c170921b13a10aea797f38ccddf2
Tressa Danvers <TDanvers@protonmail.ch>
2024-01-30 14:46:22 -0500

    [c/reverse-string] first submission

--- c/reverse-string/reverse_string.c
+++ c/reverse-string/reverse_string.c
@@ -1,1 +1,13 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 #include "reverse_string.h"
+
+char* reverse(const char *value) {
+    size_t len = strlen(value);
+    char* reversed_value = malloc(sizeof(char)*(strlen(value)+1));
+    reversed_value[len] = '\0';
+    for (size_t i = 0; i < len; i++)
+        reversed_value[i] = value[len-i-1];
+    return reversed_value;
+}
