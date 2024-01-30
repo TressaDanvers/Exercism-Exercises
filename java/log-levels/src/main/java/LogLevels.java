67117233825e5b20bed7ab4f1ee675d972053ecc
Tressa Danvers <TDanvers@protonmail.ch>
2024-01-30 15:28:49 -0500

    [java/log-levels] first submission

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,22 @@
+import java.util.Arrays;
+
 public class LogLevels {
-    
-    public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
-    }
+  public static String message(String logLine) {
+    return logLine
+        .replaceFirst("^\\[INFO]:\\s+", "")
+        .replaceFirst("^\\[WARNING]:\\s+", "")
+        .replaceFirst("^\\[ERROR]:\\s+", "")
+        .replaceAll("\\s+$", "");
+  }
 
-    public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
-    }
+  public static String logLevel(String logLine) {
+    if (logLine.startsWith("[INFO]: ")) return "info";
+    else if (logLine.startsWith("[WARNING]: ")) return "warning";
+    else if (logLine.startsWith("[ERROR]: ")) return "error";
+    else throw new IllegalArgumentException("\"" + logLine + "\" does not contain a log-level tag.");
+  }
 
-    public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
-    }
+  public static String reformat(String logLine) {
+    return message(logLine) + " (" + logLevel(logLine) + ")";
+  }
 }
