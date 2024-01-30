a70f62c402ab49d750161213b43454269e838aa6
Tressa Danvers <TDanvers@protonmail.ch>
2024-01-30 16:10:00 -0500

    [java/squaky-clean] first submission

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,23 @@
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+
 class SqueakyClean {
-    static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
-    }
+  static String clean(String identifier) {
+    AtomicBoolean convertCamel = new AtomicBoolean(false);
+
+    return identifier.chars()
+      .mapToObj(c -> (char)c)
+      .map(c -> {
+        var token = c == ' ' ? "_"
+          : Character.isISOControl(c) ? "CTRL"
+          : !Character.isAlphabetic(c) || ('α' <= c && c <= 'ω') ? ""
+          : convertCamel.get() ? String.valueOf(Character.toUpperCase(c))
+          : String.valueOf(c);
+
+        convertCamel.set(c == '-');
+
+        return token;
+      })
+      .collect(Collectors.joining());
+  }
 }
