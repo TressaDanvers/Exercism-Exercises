a6f2d113f1916ddb055bfd15fc20e116942c0800
Tressa Danvers <TDanvers@protonmail.ch>
2024-01-31 18:00:09 -0500

    [c++/last-will] first submission

--- cpp/last-will/last_will_test.cpp
+++ cpp/last-will/last_will_test.cpp
@@ -1,24 +1,14 @@
-// Trick to let the code compile, even if the function has not been implemented:
 namespace estate_executor {
     int assemble_account_number(int) __attribute__((weak));
     int assemble_code() __attribute__((weak));
 }
 
 #include "last_will.cpp"
-#ifdef EXERCISM_TEST_SUITE
-#include <catch2/catch.hpp>
-#else
 #include "test/catch.hpp"
-#endif
 
 using namespace std;
 
 TEST_CASE("Family secrets have not been altered", "[task_1]") {
-    // We cannot test the existence of a namespace in the compiled 
-    // Code.
-    // This test merely checks if the numbers in the file have 
-    // been changed. They have to be correct for the test to work.
-
     REQUIRE(zhang::bank_number_part(1) == 8541);
     REQUIRE(zhang::bank_number_part(3) == 8541 * 3 % 10'000);
     REQUIRE(khan::bank_number_part(1) == 4142);
@@ -39,8 +29,6 @@ TEST_CASE("Account number assembly function exists in correct namespace", "[task
     REQUIRE_NOTHROW(estate_executor::assemble_account_number(0));
 }
 
-#if defined(EXERCISM_RUN_ALL_TESTS)
-
 TEST_CASE("Account number assembly works correctly", "[task_2]") {
     int account_with_secret_1{16706};
     int account_with_secret_23{14238};
@@ -58,5 +46,3 @@ TEST_CASE("Code fragments fit correctly", "[task_3]") {
 
     REQUIRE(estate_executor::assemble_code() == final_code);
 }
-
-#endif
