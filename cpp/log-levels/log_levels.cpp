c605878df8c7e33c34eeb42528700efbd822ff5a
Tressa Danvers <TDanvers@protonmail.ch>
2024-02-01 15:20:29 -0500

    [c++/log-levels] first submission

--- cpp/log-levels/log_levels.cpp
+++ cpp/log-levels/log_levels.cpp
@@ -1,5 +1,19 @@
 #include <string>
 
 namespace log_line {
+  std::string message(const std::string& str) {
+    auto str_len = str.length();
+    auto end_tag = str.find_first_of(']');
+    return str.substr(end_tag + 3, str_len - end_tag - 3);
+  }
 
-} // namespace log_line
+  std::string log_level(const std::string& str) {
+    auto start_tag = str.find_first_of('[');
+    auto end_tag = str.find_first_of(']');
+    return str.substr(start_tag + 1, end_tag - 1);
+  }
+
+  std::string reformat(const std::string& str) {
+    return message(str) + " (" + log_level(str) + ")";
+  }
+}
