dfbcbbe7df73120f4ceec528e3f1fd219afa4a16
Tressa Danvers <TDanvers@protonmail.ch>
2024-02-01 15:30:00 -0500

    [c++/reverse-string] first submission

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,5 +1,10 @@
+#include <algorithm>
 #include "reverse_string.h"
 
 namespace reverse_string {
-
-}  // namespace reverse_string
+  std::string reverse_string(const std::string& str) {
+    auto reversed(str);
+    std::reverse(reversed.begin(), reversed.end());
+    return reversed;
+  }
+}
