21aa927bd8b7e2f76c2bc44d558f79318aad2ceb
Tressa Danvers <TDanvers@protonmail.ch>
2024-02-01 20:56:43 -0500

    [r/raindrops] first submission

--- r/raindrops/raindrops.R
+++ r/raindrops/raindrops.R
@@ -1,3 +1,8 @@
 raindrops <- function(number) {
-  
+  running <- ""
+  if (number %% 3 == 0) running <- "Pling"
+  if (number %% 5 == 0) running <- sprintf("%s%s", running, "Plang")
+  if (number %% 7 == 0) running <- sprintf("%s%s", running, "Plong")
+  if (running == "") running <- sprintf("%i", number)
+  return (running)
 }
