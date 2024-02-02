efb9c4ea1073f4f56013eb36e30fc018c7a7f3a1
Tressa Danvers <TDanvers@protonmail.ch>
2024-02-01 20:42:14 -0500

    [ruby/raindrops] second submission

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -2,14 +2,9 @@ class Raindrops
   # @return [String]
   def self.convert(i)
     running = ""
-    if i % 3 == 0
-      running += "Pling" end
-    if i % 5 == 0
-      running += "Plang" end
-    if i % 7 == 0
-      running += "Plong" end
-    if running == ""
-      running += String(i) end
-    running
+    running += "Pling" if i % 3 == 0
+    running += "Plang" if i % 5 == 0
+    running += "Plong" if i % 7 == 0
+    running.empty? ? String(i) : running
   end
 end
