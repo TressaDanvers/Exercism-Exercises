1d1861691fcf44fb90ed988f3d57828e51247d6c
Tressa Danvers <TDanvers@protonmail.ch>
2024-02-01 22:49:00 -0500

    [kotlin/raindrops] second submission

--- kotlin/raindrops/src/main/kotlin/Raindrops.kt
+++ kotlin/raindrops/src/main/kotlin/Raindrops.kt
@@ -2,15 +2,8 @@ fun never(): Nothing = throw IllegalStateException()
 
 object Raindrops {
     fun convert(n: Int) =
-			listOf(3, 5, 7)
-				.filter { n % it == 0 }
-				.joinToString("") {
-					when (it) {
-						3 -> "Pling"
-						5 -> "Plang"
-						7 -> "Plong"
-						else -> never()
-					}
-				}
+			sequenceOf(3 to "Pling", 5 to "Plang", 7 to "Plong")
+				.filter { (it, _) -> n % it == 0 }
+				.joinToString("") { (_, it) -> it }
 				.ifEmpty { "$n" }
 }
